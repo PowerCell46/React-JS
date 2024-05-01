0d25fdaf209c9ecd0b385e5193c97532a316c0ff
Peter Gerdzhikov <makotsevo.fan@gmail.com>
2024-05-01 09:54:56 +0300

    workshop init

---
+++ Workshop Routing/src/App.jsx
@@ -0,0 +1,16 @@
+import Navigation from "./components/Navigation"
+
+function App() {
+
+  return (
+    <>
+        <Navigation/>
+        
+        <main id="main-content">
+        
+        </main>
+    </>
+  )
+}
+
+export default App
